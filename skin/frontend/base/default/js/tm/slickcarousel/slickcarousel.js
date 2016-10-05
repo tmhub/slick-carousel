de1766b6d380bbb2235ff13530ed80c8465f1a89
Roman Chumak <chumakrom@gmail.com>
2016-10-05 16:49:36 +0300

    Slick init script and styles for default and rwd added

--- skin/frontend/base/default/js/tm/slickcarousel/slickcarousel.js
+++ skin/frontend/base/default/js/tm/slickcarousel/slickcarousel.js
@@ -1,1 +1,16 @@
-/* slickcarousel init script */
+document.observe("dom:loaded", function() {
+    if (typeof jQuery === 'undefined') {
+        console.warn('Slick Carousel can not find jQuery');
+        return;
+    }
+    // init slick
+    jQuery('div[data-slick]').slick();
+    // init slick wrapper
+    jQuery.each(jQuery('div[data-slick-wrapper]'), function() {
+        var wrapper = jQuery(this);
+        var config = wrapper.data('slick-wrapper');
+        if (config.el) {
+            wrapper.find(config.el).slick(config);
+        }
+    });
+});
